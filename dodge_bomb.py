006c0e24373f7c3f6f13b3a4fc1dd025ba9df7b9
Keiya Takamatsu <c0a241427a@edu.teu.ac.jp>
2025-04-22 14:50:54 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,22 @@ DELTA ={
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果タプル（横, 縦）
+    画面内ならTrue,画面外ならFalse
+    """
+    
+    yoko, tate= True, True # 横, 縦方向の変数
+    # 横方向判定
+    if rct.left < 0 or WIDTH <rct.right: # 画面内だったら
+        yoko = False
+    # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom: # 画面外だったら
+        tate = False
+    return yoko, tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -43,8 +59,15 @@ def main():
                 sum_mv[0] += mv[0] # 左右方向
                 sum_mv[1] += mv[1] # 上下方向
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True): # 画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 画面内に戻す
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: # 左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate: # 上下どちらかにはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
